992918c70543233868b19cbe8f470339b1d6a184
massercapper67 <alvarobardajirobles@gmail.com>
2024-11-15 15:58:44 +0100

    Removed main class

--- jdbc-connector/src/main/java/masser/jdbc/Main.java
+++
@@ -1,9 +0,0 @@
-package masser.jdbc;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Connector connector = new Connector("localhost", "3306", "root", "", "reformas");
-
-        connector.executeQuery("SELECT * FROM conductores");
-    }
-}
